23a71d542210009312717d60acda8d422c86a369
jshaptic <eugene.shatilo@gmail.com>
2019-07-09 16:43:26 +0300

    Removed spaces

--- src/test/java/com/github/jshaptic/project_name/LibraryTest.java
+++ src/test/java/com/github/jshaptic/project_name/LibraryTest.java
@@ -9,24 +9,24 @@ import org.testng.annotations.Test;
 
 public class LibraryTest extends Assert
 {
-    @Test
-    public void someLibraryMethodReturnsTrue()
-    {
-        Library classUnderTest = new Library();
-        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
-    }
-    
-    @Test(expectedExceptions = FileSystemNotFoundException.class)
-    public void testReadResourceAsURI() throws IOException, URISyntaxException
-    {
-        Library classUnderTest = new Library();
-        assertEquals(classUnderTest.readResourceAsURI(), "some data");
-    }
-    
-    @Test
-    public void testReadResourceAsStream() throws IOException
-    {
-        Library classUnderTest = new Library();
-        assertEquals(classUnderTest.readResourceAsStream(), "some data");
-    }
+	@Test
+	public void someLibraryMethodReturnsTrue()
+	{
+		Library classUnderTest = new Library();
+		assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+	}
+	
+	@Test(expectedExceptions = FileSystemNotFoundException.class)
+	public void testReadResourceAsURI() throws IOException, URISyntaxException
+	{
+		Library classUnderTest = new Library();
+		assertEquals(classUnderTest.readResourceAsURI(), "some data");
+	}
+	
+	@Test
+	public void testReadResourceAsStream() throws IOException
+	{
+		Library classUnderTest = new Library();
+		assertEquals(classUnderTest.readResourceAsStream(), "some data");
+	}
 }
