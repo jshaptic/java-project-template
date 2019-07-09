11eeb3e5c1757fe17957af4adcb3fa9de71f7d28
jshaptic <eugene.shatilo@gmail.com>
2019-07-09 16:42:45 +0300

    Added test for reading resource as stream

--- src/main/java/com/github/jshaptic/project_name/Library.java
+++ src/main/java/com/github/jshaptic/project_name/Library.java
@@ -1,6 +1,9 @@
 package com.github.jshaptic.project_name;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -14,8 +17,20 @@ public class Library
 		return true;
 	}
 	
-	public String readResourceByURI() throws IOException, URISyntaxException
+	public String readResourceAsURI() throws IOException, URISyntaxException
 	{
 		return new String(Files.readAllBytes(Paths.get(ClassLoader.getSystemClassLoader().getResource(resource).toURI())));
+	}
+	
+	public String readResourceAsStream() throws IOException
+	{
+		try
+		(
+			InputStream in = ClassLoader.getSystemResourceAsStream(resource);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		)
+		{
+			return reader.readLine();
+		}
 	}
 }
